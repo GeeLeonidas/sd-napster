353a814a075cab7ff49a16dba8b4bd131666cfb1
Guilherme <geeleonidas@pm.me>
2023-06-07 20:31:06 -0300

    Change target JDK version to 17

--- src/main/java/br/dev/gee/sdnapster/Main.java
+++ src/main/java/br/dev/gee/sdnapster/Main.java
@@ -1,9 +1,6 @@
 package br.dev.gee.sdnapster;
 
-import java.io.BufferedReader;
 import java.io.IOException;
-import java.io.InputStreamReader;
-import java.io.PrintWriter;
 import java.net.ServerSocket;
 import java.net.Socket;
 
