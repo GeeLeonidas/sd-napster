f462691b1b0ba5044d6c82a972a1605b7b8b422d
Guilherme <geeleonidas@pm.me>
2023-06-15 18:07:24 -0300

    Add MD5

---
+++ src/main/java/br/dev/gee/sdnapster/common/MD5.java
@@ -0,0 +1,25 @@
+package br.dev.gee.sdnapster.common;
+
+import java.security.MessageDigest;
+import java.security.NoSuchAlgorithmException;
+
+import javax.annotation.Nonnull;
+
+public class MD5 {
+    final byte[] hashValue;
+
+    public MD5(@Nonnull byte[] binaryData) throws NoSuchAlgorithmException {
+        MessageDigest md = MessageDigest.getInstance("MD5");
+        hashValue = md.digest(binaryData);
+    }
+
+    @Override
+    public boolean equals(Object arg0) {
+        try {
+            MD5 other = (MD5) arg0;
+            return other.hashValue.equals(this.hashValue);
+        } catch (Exception exception) {
+            return false;
+        }
+    }
+}
