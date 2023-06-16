06dc1701c8bffb66f90c71cffb0eab0dcb176bc6
Guilherme <geeleonidas@pm.me>
2023-06-15 22:56:15 -0300

    Last commit

--- src/main/java/br/dev/gee/sdnapster/common/ChunkMapService.java
+++ src/main/java/br/dev/gee/sdnapster/common/ChunkMapService.java
@@ -7,7 +7,7 @@ import java.util.Set;
 import javax.annotation.Nonnull;
 import javax.annotation.Nullable;
 
-public interface RemoteChunkMap extends Remote {
+public interface ChunkMapService extends Remote {
     public void put(@Nonnull String fromFile, @Nonnull Set<MD5> chunkHashes) throws RemoteException;
     public @Nullable Set<MD5> get(@Nonnull String fromFile) throws RemoteException;
     public @Nonnull Set<String> search(@Nonnull String fromHostsFile, @Nonnull MD5 forChunkHash) throws RemoteException;
--- src/main/java/br/dev/gee/sdnapster/tracker/TrackerMain.java
+++ src/main/java/br/dev/gee/sdnapster/tracker/TrackerMain.java
@@ -1,23 +1,73 @@
 package br.dev.gee.sdnapster.tracker;
 
-import java.net.MalformedURLException;
 import java.rmi.Naming;
 import java.rmi.RemoteException;
+import java.rmi.server.RemoteServer;
+import java.rmi.server.ServerNotActiveException;
+import java.rmi.server.UnicastRemoteObject;
+import java.util.Enumeration;
+import java.util.HashMap;
+import java.util.HashSet;
+import java.util.Set;
+import java.util.concurrent.ConcurrentHashMap;
 
-public class TrackerMain {
+import javax.annotation.Nonnull;
+import javax.annotation.Nullable;
 
-    private TrackerMain() {
+import br.dev.gee.sdnapster.common.ChunkMapService;
+import br.dev.gee.sdnapster.common.MD5;
+
+public class TrackerMain extends UnicastRemoteObject implements ChunkMapService {
+    private static ConcurrentHashMap<String, HashMap<String, Set<MD5>>> chunkMapByHost;
+
+    public TrackerMain() throws RemoteException {
+        super();
+    }
+
+    @Override
+    public void put(@Nonnull String fromFile, @Nonnull Set<MD5> chunkHashes) throws RemoteException {
         try {
-            RemoteChunkMapImpl remoteChunkMapImpl = new RemoteChunkMapImpl();
-            Naming.rebind("RMI://127.0.0.1:1020/ChunkMapService", remoteChunkMapImpl); 
-        } catch (RemoteException exception) {
-            exception.printStackTrace();
-        } catch (MalformedURLException exception) {
+            final String clientHost = RemoteServer.getClientHost();
+            HashMap<String, Set<MD5>> newValue =
+                chunkMapByHost.getOrDefault(clientHost, new HashMap<>());
+            newValue.put(fromFile, chunkHashes);
+            chunkMapByHost.put(clientHost, newValue);
+        } catch (ServerNotActiveException exception) {
             exception.printStackTrace();
         }
     }
 
-    public static void main(String[] args) {
-        new TrackerMain();
+    @Override
+    public @Nullable Set<MD5> get(@Nonnull String fromFile) throws RemoteException {
+        try {
+            final String clientHost = RemoteServer.getClientHost();
+            if (chunkMapByHost.containsKey(clientHost))
+                return chunkMapByHost.get(clientHost).get(fromFile);
+        } catch (ServerNotActiveException exception) {
+            exception.printStackTrace();
+        }
+
+        return null;
+    }
+
+    @Override
+    public @Nonnull Set<String> search(@Nonnull String fromHostsFile, @Nonnull MD5 forChunkHash) throws RemoteException {
+        HashSet<String> result = new HashSet<>();
+        
+        Enumeration<String> clientHosts = chunkMapByHost.keys();
+        while (clientHosts.hasMoreElements()) {
+            String it = clientHosts.nextElement();
+            Set<MD5> set = chunkMapByHost.get(it).get(fromHostsFile);
+            if (set != null && set.contains(forChunkHash))
+                result.add(it);
+        }
+
+        return result;
+    }
+
+    public static void main(String args[]) throws Exception {
+        ChunkMapService chunkMapService = new TrackerMain();
+        Naming.bind("ChunkMapService", chunkMapService);
+        System.out.println("ChunkMapService bound");
     }
 }
