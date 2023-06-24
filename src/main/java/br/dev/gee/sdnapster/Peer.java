0734491998d8ef0056a89f0eac63018ddd9a394a
Guilherme <geeleonidas@pm.me>
2023-06-24 15:14:52 -0300

    Add comments all over `Peer::main`

--- src/main/java/br/dev/gee/sdnapster/Peer.java
+++ src/main/java/br/dev/gee/sdnapster/Peer.java
@@ -103,15 +103,18 @@ public class Peer {
 			Address tcpAddress = null;
 			while (!(option = Peer.readOption(scanner)).equals("EXIT")) {
 				switch (option) {
+					// Requisição JOIN
 					case "JOIN":
 						if (peerPath != null) {
 							System.out.println("Requisição JOIN já executada!");
 							continue;
 						}
+						// Entrada dos dados
 						final InetAddress tcpHost = Servidor.readHost(scanner, "TCP", Peer.DEFAULT_TCP_HOST);
 						final int tcpPort = Servidor.readPort(scanner, "TCP", Peer.DEFAULT_TCP_PORT);
 						final Path finalPeerPath = Peer.readPath(scanner);
 						final List<String> filenames = Peer.listAllFilenames(finalPeerPath);
+						// Início da Thread de upload via TCP
 						tcpThread = new Thread(() -> {
 							try (final ServerSocket socket = new ServerSocket(tcpPort, 50, tcpHost)) {
 								while (true) {
@@ -121,24 +124,30 @@ public class Peer {
 												BufferedInputStream in = new BufferedInputStream(clientSocket.getInputStream());
 												BufferedOutputStream out = new BufferedOutputStream(clientSocket.getOutputStream())
 										) {
+											// Buffer de 4 KiB para ler a stream
 											byte[] buffer = new byte[4096];
 											int bytesRead = in.read(buffer);
 											if (bytesRead == -1)
 												return;
+											// Interpretação do buffer como um array de Strings (especificação do Charset obrigatória para evitar bugs)
 											final String[] message = new String(buffer, StandardCharsets.UTF_8).split("\n");
-											if (message.length < 2 || !message[0].equals("DOWNLOAD"))
+											if (message.length < 2 || !message[0].equals("DOWNLOAD")) // 1º elemento é um header
 												return;
+											// Uso do conteúdo da mensagem de fato
 											Path filePath = FileSystems.getDefault().getPath(finalPeerPath + "/" + message[1]);
 											if (!Files.isRegularFile(filePath)) {
+												// Retorno em caso de erro
 												out.write("DOWNLOAD_ERR\n".getBytes(StandardCharsets.UTF_8));
 												return;
 											}
+											// Retorno em caso de sucesso
 											out.write("DOWNLOAD_OK\n".getBytes(StandardCharsets.UTF_8));
 											try (InputStream fileStream = Files.newInputStream(filePath)) {
 												while (!clientSocket.isClosed()) {
 													bytesRead = fileStream.read(buffer);
 													if (bytesRead == -1)
 														break;
+													// Uso direto do buffer *DEVE* ser acompanhado de `bytesRead` para evitar a leitura de lixo
 													out.write(buffer, 0, bytesRead);
 												}
 											}
@@ -152,19 +161,24 @@ public class Peer {
 							}
 						});
 						tcpThread.start();
+						// Chamada RMI `TrackerService::join`
+						// Se o valor de retorno não for "JOIN_OK", volte ao menu interativo
 						if (!tracker.join(filenames, new Address(tcpHost, tcpPort)).equals("JOIN_OK"))
 							continue;
 						peerPath = finalPeerPath;
 						tcpAddress = new Address(tcpHost, tcpPort);
 						System.out.printf("Sou peer %s:%d com arquivos %s\n", tcpHost.getHostAddress(), tcpPort, String.join(" ", filenames));
 						break;
+					// Requisição SEARCH
 					case "SEARCH":
 						if (peerPath == null) {
 							System.out.println("Execute uma requisição JOIN antes!");
 							continue;
 						}
+						// Entrada dos dados
 						System.out.print("Insira o nome do arquivo a ser procurado na rede: ");
 						final String filename = scanner.nextLine();
+						// Chamada RMI `TrackerService::search`
 						searchCache = tracker.search(filename);
 						searchedFile = filename;
 						final ArrayList<String> stringSearchCache = new ArrayList<>();
@@ -172,6 +186,7 @@ public class Peer {
 							stringSearchCache.add(address.toString());
 						System.out.printf("peers com arquivo solicitado: %s\n", String.join(" ", stringSearchCache));
 						break;
+					// Requisição DOWNLOAD
 					case "DOWNLOAD":
 						if (searchCache == null) {
 							System.out.println("Execute uma requisição SEARCH antes!");
@@ -182,6 +197,7 @@ public class Peer {
 							continue;
 						}
 						while (true) {
+							// Entrada dos dados
 							final InetAddress downloadHost = Servidor.readHost(scanner, "Download", Peer.DEFAULT_TCP_HOST);
 							final int downloadPort = Servidor.readPort(scanner, "Download", Peer.DEFAULT_TCP_PORT);
 							final Address downloadAddress = new Address(downloadHost, downloadPort);
@@ -191,29 +207,35 @@ public class Peer {
 							final Path finalDownloadFolder = peerPath;
 							final Path downloadPath = FileSystems.getDefault().getPath(finalDownloadFolder + "/" + finalSearchedFile);
 							final Address finalTcpAddress = tcpAddress;
+							// Início da Thread de download via TCP
 							new Thread(() -> {
 								try (
 										Socket serverSocket = new Socket(downloadHost, downloadPort);
 										BufferedInputStream in = new BufferedInputStream(serverSocket.getInputStream());
 										BufferedOutputStream out = new BufferedOutputStream(serverSocket.getOutputStream())
 								) {
+									// Envio formatado dos dados da requisição
 									out.write(String.format("DOWNLOAD\n%s\n", finalSearchedFile).getBytes(StandardCharsets.UTF_8));
 									byte[] buffer = new byte[4096];
 									int bytesRead = in.read(buffer);
 									if (bytesRead == -1)
 										return;
+									// Interpretação do buffer como um array de Strings (especificação do Charset obrigatória para evitar bugs)
 									final String[] message = new String(buffer, StandardCharsets.UTF_8).split("\n");
-									if (message.length < 1 || !message[0].equals("DOWNLOAD_OK"))
+									if (message.length < 1 || !message[0].equals("DOWNLOAD_OK")) // Uso apenas do header
 										return;
 									try (OutputStream fileStream = Files.newOutputStream(downloadPath)) {
 										while (!serverSocket.isClosed()) {
 											bytesRead = in.read(buffer);
 											if (bytesRead == -1)
 												break;
+											// Uso direto do buffer *DEVE* ser acompanhado de `bytesRead` para evitar a leitura de lixo
 											fileStream.write(buffer, 0, bytesRead);
 										}
 									}
 									if (!serverSocket.isClosed()) {
+										// Chamada RMI `TrackerService::update`
+										// Apenas mostre a mensagem em caso de sucesso (retorno igual a "UPDATE_OK")
 										if (tracker.update(finalSearchedFile, finalTcpAddress).equals("UPDATE_OK"))
 											System.out.printf("\nArquivo %s baixado com sucesso na pasta %s\n", finalSearchedFile, finalDownloadFolder);
 									}
